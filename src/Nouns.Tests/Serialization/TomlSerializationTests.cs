b1312f2fae8f8a1c1cb5e910f463945381e87304
Wattsy <wattsyart@protonmail.com>
2022-10-19 17:14:10 -0400

    add failing test to shore up what is needed for TOML serialization

---
+++ src/Nouns.Tests/Serialization/TomlSerializationTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Nouns.Engine.Pixel2D;
+using Xunit;
+using Nouns.Graphics.Core;
+using Tomlyn;
+using Xunit.Abstractions;
+
+namespace Nouns.Tests.Serialization
+{
+    public class TomlSerializationTests
+    {
+        private readonly ITestOutputHelper console;
+
+        public TomlSerializationTests(ITestOutputHelper console)
+        {
+            Bootstrap.Init();
+
+            this.console = console;
+        }
+
+        [Fact]
+        public void NestedObjectTests()
+        {
+            var definitions = new PixelsDefinitions();
+
+            var graphicsDevice = HeadlessGraphicsDeviceService.AcquireGraphicsDevice(out _);
+            
+            var texture = new Texture2D(graphicsDevice, 1, 1);
+            texture.SetData(new[] { Color.White });
+
+            var sprite = new Sprite(texture);
+            
+            var cel = new Cel(sprite);
+            
+            var frame = new AnimationFrame();
+            frame.layers.Add(cel);
+            
+            var animation = new Animation();
+            animation.Frames.Add(frame);
+
+            var animationSet = new AnimationSet();
+            animationSet.friendlyName = "fake";
+            animationSet.animations.Add(animation);
+
+            var position = new Position(100, 100);
+            var thing = new Thing(animationSet, position, false);
+
+            var level = new Level();
+            level.things.Add(thing);
+            definitions.levels.Add(level);
+
+            var toml = ToToml(definitions);
+            console.WriteLine(toml);
+
+            var definitionsFromToml = FromToml(toml);
+            Assert.Equal(definitionsFromToml.levels[0].things[0].AnimationSet.friendlyName, animationSet.friendlyName);
+        }
+
+        private static string ToToml(PixelsDefinitions definitions)
+        {
+            var options = new TomlModelOptions {IgnoreMissingProperties = false};
+            Assert.True(Toml.TryFromModel(definitions, out var toml, out var diagnostics, options));
+            Assert.False(diagnostics.HasErrors);
+            return toml;
+        }
+
+        private static PixelsDefinitions FromToml(string toml)
+        {
+            var options = new TomlModelOptions { IgnoreMissingProperties = false };
+            Assert.True(Toml.TryToModel<PixelsDefinitions>(toml, out var definitions, out var diagnostics, null, options));
+            Assert.NotNull(diagnostics);
+            Assert.False(diagnostics.HasErrors);
+            return definitions;
+        }
+    }
+}
