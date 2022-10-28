ee4d09a4c62ad9e5d2b1e4627427f0953ea4c810
Wattsy <wattsyart@protonmail.com>
2022-10-28 02:09:10 -0400

    add state machine wiring to Actor

--- src/NGE.Engine.Pixel2D/Actor.cs
+++ src/NGE.Engine.Pixel2D/Actor.cs
@@ -1,6 +1,25 @@
-﻿namespace NGE.Engine.Pixel2D;
+﻿using NGE.Engine.StateMachine;
 
-public class Actor
+namespace NGE.Engine.Pixel2D;
+
+public class Actor : StateMachine<UpdateContext>
 {
+    #region State Machine
 
+    public new MethodTable? StateMethods => (MethodTable?) CurrentState?.methodTable;
+
+    public new class MethodTable : StateMachine<UpdateContext>.MethodTable
+    {
+        public delegate void UpdateDelegate(Actor self, UpdateContext updateContext);
+
+        // ReSharper disable once InconsistentNaming
+        public UpdateDelegate Update = null!;
+    }
+
+    #endregion
+
+    public virtual void Update(UpdateContext updateContext)
+    {
+        StateMethods?.Update(this, updateContext);
+    }
 }
