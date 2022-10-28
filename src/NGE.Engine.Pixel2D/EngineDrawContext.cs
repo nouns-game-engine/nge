d6f07b068d477894af2b52a73b75bb312e2f8cc2
Wattsy <wattsyart@protonmail.com>
2022-10-28 18:05:43 -0400

    remove unused class

--- src/NGE.Engine.Pixel2D/EngineDrawContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Xna.Framework.Graphics;
-
-namespace NGE.Engine.Pixel2D;
-
-public sealed class EngineDrawContext : DrawContext
-{
-    public EngineDrawContext(SpriteBatch sb, SpriteFont defaultFont, Texture2D uiTexture, Texture2D whitePixel) 
-        : base(sb, defaultFont, uiTexture, whitePixel) { }
-}
