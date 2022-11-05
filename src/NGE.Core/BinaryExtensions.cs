65e96bd1a3ca24dd68f9af13e3f26e112ec85338
Wattsy <wattsyart@protonmail.com>
2022-11-05 13:53:30 -0400

    add rectangle and point serialization helpers

--- src/NGE.Core/BinaryExtensions.cs
+++ src/NGE.Core/BinaryExtensions.cs
@@ -26,5 +26,49 @@ namespace NGE.Core
             bw.Write(vector2.X);
             bw.Write(vector2.Y);
         }
+
+        public static Color ReadColor(this BinaryReader br)
+        {
+            Color c = new Color();
+            c.PackedValue = br.ReadUInt32();
+            return c;
+        }
+
+        public static void Write(this BinaryWriter bw, Color c)
+        {
+            bw.Write(c.PackedValue);
+        }
+
+        public static Point ReadPoint(this BinaryReader br)
+        {
+            Point p;
+            p.X = br.ReadInt32();
+            p.Y = br.ReadInt32();
+            return p;
+        }
+
+        public static void Write(this BinaryWriter bw, Point p)
+        {
+            bw.Write(p.X);
+            bw.Write(p.Y);
+        }
+
+        public static Rectangle ReadRectangle(this BinaryReader br)
+        {
+            Rectangle r;
+            r.X = br.ReadInt32();
+            r.Y = br.ReadInt32();
+            r.Width = br.ReadInt32();
+            r.Height = br.ReadInt32();
+            return r;
+        }
+
+        public static void Write(this BinaryWriter bw, Rectangle r)
+        {
+            bw.Write(r.X);
+            bw.Write(r.Y);
+            bw.Write(r.Width);
+            bw.Write(r.Height);
+        }
     }
 }
